3a7cae80c6ce8084ab058b9488dd06dd19353a6f
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-07-05 14:47:26 -0300

    last

--- components/Forecast.jsx
+++ components/Forecast.jsx
@@ -4,8 +4,8 @@ const Forecast = ({ title, items }) => {
   return (
     <section className="w-full h-full flex flex-col items-center p-10 bg-the-black sm:w-2/3 sm:h-96  text-my-white">
       <div className="grid grid-cols-2 gap-6 mt-5 sm:flex">
-        {items.map((item) => (
-          <div className="flex flex-col gap-6 p-4 bg-my-blue">
+        {items.map((item, index) => (
+          <div key={index} className="flex flex-col gap-6 p-4 bg-my-blue">
             <p>{item.title}</p>
             <div className="w-full flex items-center justify-center">
               <img
